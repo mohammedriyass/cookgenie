78212742c5a98d02b18f09612ee2c29a50a22df7
mohammedriyass <kit28.24bcb033@gmail.com>
2026-05-24 01:23:30 +0530

    Initial commit - CookGenie Spring Boot setup

---
+++ src/test/java/com/cookgenie/cookgenie/CookgenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookgenie.cookgenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookgenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
